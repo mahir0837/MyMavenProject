73e3e60641c7cc4e2a8a4d3ed2552859c17d0b5d
owner <mahirsarac89@gmail.com>
2023-02-17 14:14:08 -0500

    ashbas

--- src/main/java/com/sarac/mahir.java
+++ src/main/java/com/sarac/mahir.java
@@ -1,5 +1,10 @@
 package com.sarac;
 
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
 public interface mahir {
 
+    LinkedList<BigDecimal> method2(List<Long> longList);
 }
---
+++ src/main/java/com/sarac/mahir2.java
@@ -0,0 +1,21 @@
+package com.sarac;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
+public class mahir2 {
+
+    public static void main(String[] args) {
+
+        mahir abc=new mahir() {
+            @Override
+            public LinkedList<BigDecimal> method2(List<Long> longList) {
+                return null;
+            }
+
+        };
+
+        mahir abcd=longList -> new LinkedList<>();
+    }
+}
